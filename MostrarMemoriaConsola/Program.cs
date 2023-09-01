c96d49452ca9339cb72c6b4296ad96f3ca248d80
Saulm <Saulm@SAUL>
2023-08-31 22:02:30 -0600

    Agregar archivos de proyecto.

---
+++ MostrarMemoriaConsola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostrarMemoriaConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la ruta de la carpeta:");
+            string selectedPath = Console.ReadLine();
+
+            if (Directory.Exists(selectedPath))
+            {
+                string[] files = Directory.GetFiles(selectedPath);
+
+                Console.WriteLine("Archivos en la carpeta:");
+                foreach (string filePath in files)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    Console.WriteLine(fileName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("La ruta no es válida o la carpeta no existe.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
